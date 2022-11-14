5cb35eb4fa4b7b220ee96d8e10056f917646b579
Galaxy092 <096polideth@gmail.com>
2022-11-14 08:48:28 +0700

    create body section

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,7 +10,7 @@ export default function signin({providers}) {
         {Object.values(providers).map(provider => (
           <div key={provider.name} className="flex flex-col items-center">
             <img className='w-52 object-cover' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png" alt="" />
-            <p className='text-sm my-10 text-center'>Test Google Clone with NextJS</p>
+            <p className='text-sm my-10 text-center'>Test Google Clone Login with NextJS</p>
             <button className='bg-red-400 rounded-lg text-white p-3 hover:bg-red-500' onClick={() => signIn(provider.id, {callbackUrl: "/"})}>Sign in with {provider.name}</button>
           </div>
         ))}
