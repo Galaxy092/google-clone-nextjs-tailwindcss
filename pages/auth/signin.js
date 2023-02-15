4f619b6c8731dea02c79549fa6b6eb24842512a8
Galaxy092 <096polideth@gmail.com>
2023-02-15 09:40:35 +0700

    Update to Image Component

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import Header from "../../components/Header"
-import { getProviders, signIn } from "next-auth/react"
+import React from 'react';
+import Header from '../../components/Header';
+import { getProviders, signIn } from 'next-auth/react';
+import Image from 'next/image';
 
-export default function signin({providers}) {
+export default function signin({ providers }) {
   return (
     <>
       <Header />
       <div className="mt-40">
-        {Object.values(providers).map(provider => (
+        {Object.values(providers).map((provider) => (
           <div key={provider.name} className="flex flex-col items-center">
-            <img className='w-52 object-cover' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png" alt="" />
-            <p className='text-sm my-10 text-center'>Test Google Clone Login with NextJS</p>
-            <button className='bg-red-400 rounded-lg text-white p-3 hover:bg-red-500' onClick={() => signIn(provider.id, {callbackUrl: "/"})}>Sign in with {provider.name}</button>
+            <Image
+              priority
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-52 object-cover"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png"
+              alt=""
+            />
+            <p className="text-sm my-10 text-center">
+              Test Google Clone Login with NextJS
+            </p>
+            <button
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+              onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            >
+              Sign in with {provider.name}
+            </button>
           </div>
         ))}
       </div>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps() {
   const providers = await getProviders();
   return {
     props: { providers },
-  }
+  };
 }
