aba5744c3e88ac8ffb79397feeba8267c211f111
Galaxy092 <096polideth@gmail.com>
2023-02-15 09:40:07 +0700

    Update to Image Component

--- components/User.js
+++ components/User.js
@@ -1,11 +1,12 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image";
 
 export default function User({className}) {
   const { data: session } = useSession();
   if(session) {
     return (
       <>
-        <img onClick={signOut} src={session.user.image} alt="user-image" className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`} />
+        <Image width={40} height={40} onClick={signOut} src={session.user.image} alt="user-image" className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`} />
       </>
     )
   }
